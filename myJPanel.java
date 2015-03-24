3f2bffa9210497b3639baadf75e11d3918fc7e37
Jeff <Jeff@75.102.66.140>
2015-03-23 21:19:26 -0400

    created some panels

--- myJPanel.java
+++ myJPanel.java
@@ -5,8 +5,10 @@
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 /**
@@ -15,28 +17,26 @@ import javax.swing.JPanel;
  */
 public class myJPanel extends JPanel implements ActionListener
 {
-        
 	myJPanel1 p1;
-	myJPanel1 p2;
+	myJPanel2 p2;
         
 	public myJPanel()
 	{
-		setBackground(Color.green);
-		setLayout(new BorderLayout());
-
+                setLayout(new BorderLayout());
 		p1 = new myJPanel1();
                 p1.playButton.addActionListener(this);
 		add(p1,"Center");
-}
+       }
         
     public void actionPerformed(ActionEvent event) 
     {   
             Object obj = event.getSource();   
                  if (obj == p1.playButton)
      	 {
+                p2=new myJPanel2();
      	 	remove(p1);
 		add(p2,"Center");
-     	 	validate();
+                validate();
                 repaint();
      	 }       
     }
